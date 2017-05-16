a15a1547310e637ee229f6c6b542543ccbfaa714
Kem Crimmins <kem.crimmins@gmail.com>
2017-05-16 11:23:27 -0400

    Insertion sort implementation

---
+++ insertionSort.js
@@ -0,0 +1,13 @@
+var insert = function(array, rightIndex, value) {
+    var key;
+    for(key = rightIndex; key >= 0 && array[key] > value; key--) {
+        array[key + 1] = array[key];
+    }
+    array[key + 1] = value;
+};
+
+var insertionSort = function(array) {
+    for (var i = 1; i < array.length; i++) {
+        insert(array, i - 1, array[i]);   
+    }    
+};
